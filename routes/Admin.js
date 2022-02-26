f3a7a1ddb8dc69e137431649a4f238da1fb74d44
fahadpj23 <fahadpj23@gmail.com>
2022-02-26 11:04:12 +0530

    password hash

--- routes/Admin.js
+++ routes/Admin.js
@@ -1,14 +1,26 @@
 const express=require('express')
 const router = express.Router()
 const con=require('../database')
+var bodyParser=require("body-parser");
+var jsonParser=bodyParser.json();
+const bcrypt=require('bcrypt')
 router.get("/adminlogin",function(req,res)
  {
-    searchqr=`SELECT * FROM admin  where username='${req.query.username}' AND password='${req.query.password}'`
+    searchqr=`SELECT * FROM admin  where username='${req.query.username}' `
+    console.log(req.query.password)
     con.query(searchqr,(err,result,fields)=>{
 
       if(err) throw(err);
-      res.send(result)
+      console.log(result[0].password)
+      try{
+        bcrypt.compare(result[0].password,req.query.password)
+        res.send("success")
+      }
+      catch{
+        res.send("password is incorrect")
+      }
   }) 
   
 })
+
 module.exports=router;
--- routes/OrderProduct.js
+++ routes/OrderProduct.js
@@ -5,22 +5,23 @@ const router = express.Router()
 const con=require('../database')
 router.post("/orders",jsonParser,function(req,res)
     {
-        let product=req.body;
-        console.log(product)
-        if(product.phone=="")
-        {
-           res.send("phone number is null")
-        }
-        else
-        {
-        addqr=`insert into productorder ( productid, productname,date, qty, price, total, customername, phone, pincode, address ) values ('${product.productid}','${product.productname}','${product.date}','${product.qty}','${product.price}','${product.total}','${product.customername}','${product.phone}','${product.pincode}','${product.address}')`;
+        let orderinfo=req.body;
+        console.log(orderinfo)
+        let products=JSON.parse(orderinfo.product)
+       Object.values( products).map((item,key)=>{
+          
+      
+        
+        addqr=`insert into productorder ( productid, productname,date, qty, price, total, customername, phone, pincode, address ) values ('${item.productid}','${item.name}','dssd','${item.qty}','${item.price}','${item.price*item.qty}','${orderinfo.name}','${orderinfo.phone}','${orderinfo.pincode}','${orderinfo.address}')`;
             con.query(addqr,(err,result)=>{
 
               if(err) throw (err);
             else res.send("successfully added")
+            
           })
-        }
+          console.log(addqr)
+        })
        
     }
     )
-module.exports=router;
+module.exports=router; 
