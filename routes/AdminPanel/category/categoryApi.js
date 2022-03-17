22440e3f429c9af3ab02f355f8da48eb8409f68e
fahadpj23 <fahadpj23@gmail.com>
2022-03-17 14:03:14 +0530

    catgeory

--- routes/AdminPanel/category/categoryApi.js
+++ routes/AdminPanel/category/categoryApi.js
@@ -2,11 +2,15 @@ const express=require('express')
 const router = express.Router()
 const con=require('../../../database')
 const validateToken=require("../../../middlewares/authmiddelware")
+var bodyParser=require("body-parser");
+var jsonParser=bodyParser.json();
+var parseUrlencoded = bodyParser.urlencoded({ extended: true });  
+const {check,validationResult}=require('express-validator');
 
-router.get("/getCatgeory",(req,res)=>
+router.get("/getAttribute",(req,res)=>
     {
        
-        categorysearchqr=`SELECT * FROM category`
+        categorysearchqr=`SELECT * FROM attribute`
             con.query(categorysearchqr,(err,result,fields)=>
             {
             
@@ -16,6 +20,22 @@ router.get("/getCatgeory",(req,res)=>
              })
 
     })
+    router.post("/categoryAdd",parseUrlencoded,(req,res)=>
+    {
+       
+        // categorysearchqr=`SELECT * FROM attribute`
+        //     con.query(categorysearchqr,(err,result,fields)=>
+        //     {
+            
+        //     if(err) throw(err);
+        //     res.send(result)
+       
+        //      })
+        console.log(req.body)
+
+    })
+
+    
    
   
 
