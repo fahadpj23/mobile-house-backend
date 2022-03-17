6473d9be0c2a5d3ed6f8f81502b15263394e8d56
fahadpj23 <fahadpj23@gmail.com>
2022-03-17 18:23:00 +0530

    catgeory

--- routes/AdminPanel/category/categoryApi.js
+++ routes/AdminPanel/category/categoryApi.js
@@ -6,6 +6,7 @@ var bodyParser=require("body-parser");
 var jsonParser=bodyParser.json();
 var parseUrlencoded = bodyParser.urlencoded({ extended: true });  
 const {check,validationResult}=require('express-validator');
+const { object } = require('react-globally');
 
 router.get("/getAttribute",(req,res)=>
     {
@@ -20,18 +21,56 @@ router.get("/getAttribute",(req,res)=>
              })
 
     })
-    router.post("/categoryAdd",parseUrlencoded,(req,res)=>
+    router.post("/categoryAdd",
+    [
+        check('name').notEmpty(),
+        check('status').notEmpty(),
+        
+      ],
+    parseUrlencoded,(req,res)=>
     {
+        if(!error.isEmpty)
+        return res.json({error:error.array})
+        else
+        {
+        let attributes=[];
+        const{name,status}=req.body
+        let attribute=JSON.parse(req.body.attributevalues)
+        addcatgeory=`insert into category (categoryName,status) values ('${req.body.name}','${req.body.status=="active" ? 1 : 0}')`
+            con.query(addcatgeory,(err,result1)=>{
+                if(err) throw (err);
+                else
+                {
+                    Object.values(attribute).map((item,key)=>{
+                        con.query(`select * from  attribute where attributeName='${item}'`,(err,result,fields)=>
+                        {
+                        
+                        if(err) throw(err);
+                       else
+                       {
+                       
+                                
+                                addcatgeoryattribute=`insert into categoryvalue (categoryId,attributeId,attributeName) values ('${result1.insertId}','${ result[0].id}','${ result[0].attributeName}')`
+                                con.query(addcatgeoryattribute,(err,result)=>{
+                                    if(err) throw (err);
+                                    else {
+                                        
+                                        console.log("sasssa")
+                                    
+                                    }
+                            
+                            })
+                           
+                       }
+                   
+                         })
+                    })
+                }
+                })
+                
        
-        // categorysearchqr=`SELECT * FROM attribute`
-        //     con.query(categorysearchqr,(err,result,fields)=>
-        //     {
-            
-        //     if(err) throw(err);
-        //     res.send(result)
-       
-        //      })
-        console.log(req.body)
+
+            }
 
     })
 
