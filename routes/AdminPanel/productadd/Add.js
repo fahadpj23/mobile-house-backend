ce0176f34b9e48eabcccb8a7d6f0aa3a6fef8b41
fahadpj23 <fahadpj23@gmail.com>
2022-03-31 16:32:38 +0530

    product edit

--- routes/AdminPanel/productadd/Add.js
+++ routes/AdminPanel/productadd/Add.js
@@ -11,48 +11,102 @@ var parseUrlencoded = bodyParser.urlencoded({ extended: true });
 
 router.post("/productAdd",parseUrlencoded,function(req,res){
   let product=req.body
-  console.log(product)
-  const file=req.files.image
-  let columnarray=[]
-  let columnvalue=[]
-
-  file.mv(`products/images/${file.name}`)
-  addqr=`insert into products (  name,  price, mrp ,warranty, image, Brand,qty ,category) values ('${product.Name}','${product.Price}','${product.MRP}','${product.Warranty}','${file.name}','${product.Brand}','${product.qty}','${product.category}')`;
-
-  con.query(addqr,(err,result)=>{
-
-    if(err) throw (err);
-  else 
+  if(req.body.operation=="select")
   {
-   
-    // addqr=`insert into products (  name,  price, mrp ,warranty, image, Brand,qty ,category) values ('${product.Name}','${product.Price}','${product.MRP}','${product.Warranty}','${file.name}','${product.Brand}','${product.qty}','${product.category}')`;
-    // columnfetch=`SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = "${product.category}";`
-    categoryattribute=`select * from categoryvalue where  categoryId="${product.categoryid}"`
-    con.query(categoryattribute,(err,result1)=>{
-      if(err) throw (err)
-      else
-      {
-    
-       result1.map((item,key)=>{
-         columnarray.push(item.attributeName)
-        
-          columnvalue.push("'"+product[item.attributeName]+"'")
-       
-         
-       })
-       insertquery=`insert into productattribute (id,${columnarray}) values ('${result.insertId}',${columnvalue})`
-       console.log(insertquery)
-       con.query(insertquery,(err,result)=>{
-         if(err) throw (err)
-         else
-          res.json({"success":"success"})
-       })
-      }
-     
-    })
-    
+            const file=req.files.image
+            let columnarray=[]
+            let columnvalue=[]
+
+            file.mv(`products/images/${file.name}`)
+            addqr=`insert into products (  name,  price, mrp ,warranty, image, Brand,qty ,category) values ('${product.Name}','${product.Price}','${product.MRP}','${product.Warranty}','${file.name}','${product.Brand}','${product.qty}','${product.category}')`;
+
+            con.query(addqr,(err,result)=>{
+
+              if(err) throw (err);
+            else 
+            {
+            
+              // addqr=`insert into products (  name,  price, mrp ,warranty, image, Brand,qty ,category) values ('${product.Name}','${product.Price}','${product.MRP}','${product.Warranty}','${file.name}','${product.Brand}','${product.qty}','${product.category}')`;
+              // columnfetch=`SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = "${product.category}";`
+              categoryattribute=`select * from categoryvalue where  categoryId="${product.categoryid}"`
+              con.query(categoryattribute,(err,result1)=>{
+                if(err) throw (err)
+                else
+                {
+              
+                result1.map((item,key)=>{
+                  columnarray.push(item.attributeName)
+                  
+                    columnvalue.push("'"+product[item.attributeName]+"'")
+                
+                  
+                })
+                insertquery=`insert into productattribute (id,${columnarray}) values ('${result.insertId}',${columnvalue})`
+                console.log(insertquery)
+                con.query(insertquery,(err,result)=>{
+                  if(err) throw (err)
+                  else
+                    res.json({"success":"success"})
+                })
+                }
+              
+              })
+              
+            }
+          })
   }
-})
+  else
+  {
+   console.log(req.body.operationid)
+    let columnarray=[]
+    let columnvalue=[]
+    if(req.files)
+    {
+    const file=req.files.image
+    file.mv(`products/images/${file.name}`)
+    addqr=`UPDATE products SET  name='${product.Name}' , price='${product.Price}', mrp='${product.MRP}' ,warranty='${product.Warranty}', image='${file.name}', Brand='${product.Brand}',qty='${product.qty}' ,category='${product.category}' where id='${product.operationid}'`;
+    }
+    else
+    {
+      addqr=`UPDATE products SET  name='${product.Name}' , price='${product.Price}', mrp='${product.MRP}' ,warranty='${product.Warranty}',  Brand='${product.Brand}',qty='${product.qty}' ,category='${product.category}' where id='${product.operationid}'`;
+ 
+    }
+    con.query(addqr,(err,result)=>{
+
+      if(err) throw (err);
+    else 
+    {
+    
+      
+      categoryattribute=`select * from categoryvalue where  categoryId="${product.categoryid}"`
+      con.query(categoryattribute,(err,result1)=>{
+        if(err) throw (err)
+        else
+        {
+      
+        result1.map((item,key)=>{
+          let updatequ=item.attributeName+ "="+ "'" +product[item.attributeName] + "'"
+          columnarray.push(updatequ)
+          
+           
+        
+          
+        })
+        
+        insertquery=`UPDATE productattribute SET  ${columnarray.toString()} where id='${product.operationid}'`
+        console.log(insertquery)
+        con.query(insertquery,(err,result)=>{
+          if(err) throw (err)
+          else
+            res.json({"success":"success"})
+        })
+        }
+      
+      })
+      
+    }
+  })
+  }
 })
 
 router.get('/getcategoryAttribute',function(req,res){
@@ -97,8 +151,36 @@ router.get('/getProduct',(req,res)=>{
 })
 router.get('/productdetails',(req,res)=>{
 
-  console.log(req.query)
-
+  getProduct=`select * from products  where id=${req.query.productId}`
+    con.query(getProduct,(err,result)=>{
+       if(err) throw (err)
+       else
+      {
+        getProduct=`select * from productattribute  where id=${req.query.productId}`
+        con.query(getProduct,(err1,result1)=>{
+           if(err) throw (err)
+           else
+          {
+            console.log(Object.entries(result1[0]).length)
+           Object.entries(result1[0]).map((item,key)=>{
+            if(item[1]!=null)
+            {
+              result[0][item[0]]=item[1]
+            }
+              
+               console.log(key)
+               if(Object.entries(result1[0]).length ==  +key+1 )
+               {
+                 res.send(result[0])
+               }
+             
+            
+             
+           })
+          }
+        })
+      }
+    })
 })
 
 
