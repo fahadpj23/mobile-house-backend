03208d170409a2745c5d707f8e6dc94a201507e1
fahadpj23 <fahadpj23@gmail.com>
2022-05-27 19:20:34 +0530

    product add

--- routes/AdminPanel/productadd/Add.js
+++ routes/AdminPanel/productadd/Add.js
@@ -15,16 +15,17 @@ router.post("/productAdd",parseUrlencoded,function(req,res){
    
     file.mv(`products/images/${Math.round(new Date().getTime()/1000)}${file.name}`)
     imageaddqr=`insert into productimage (productId,image) values (${dbid},'${Math.round(new Date().getTime()/1000)}${file.name}')`
+    console.log(imageaddqr)
     con.query(imageaddqr,(err,result)=>{
       if(err)throw (err)
     })
   }
   let product=req.body
-console.log(product)
+
   if(req.body.operation=="" || req.body.operation=="variant" )
   { 
  
-    console.log("dsdsd")
+ 
            
             addqr=`insert into products (  name,  purchasePrice,sellingPrice,salesPrice, mrp ,warranty,  Brand,qty ,HSN,Tax,category,variantid) values ('${product.Name}','${product.purchasePrice}','${product.sellingPrice}','${product.salesPrice}','${product.MRP}','${product.Warranty}','${product.Brand}','${product.qty}','${product.HSN}','${product.Tax}','${product.categoryid}','${product.operationid}')`;
           
@@ -38,12 +39,27 @@ console.log(product)
               if(err) throw (err);
             else 
             {
-              let images=JSON.parse(product.productImage)
+              let imageblob=JSON.parse(product.productImageblob)
+              
               for(let i=0;i<=5;i++)
               {
-                let image=req.files["image"+(i)]
-                console.log(image)
-               image && productimage(image,result.insertId)
+               
+                if(imageblob[i])
+                {
+                 if(i==0)
+                 {
+                  let image=req.files.image
+                  console.log(image)
+                  image && productimage(image,result.insertId)
+                 }
+                 else
+                 {
+                  let image=req.files["image"+(i)] 
+                  console.log(i)
+                  image && productimage(image,result.insertId)
+                 }
+                
+                }
               }
               
              
@@ -59,7 +75,7 @@ console.log(product)
               }
               else
               {
-                variantupdate=`UPDATE products SET variantid="${operationid}" WHERE id="${result.insertId}" ` 
+                variantupdate=`UPDATE products SET variantid="${product.operationid}" WHERE id="${result.insertId}" ` 
                 con.query(variantupdate,(err1,result1)=>
                 {
                   if(err1) throw (err1)
@@ -101,24 +117,37 @@ console.log(product)
     console.log(product)
     let columnarray=[]
     let columnvalue=[]
-    if(req.files)
-    {
-    const file=req.files.image
-    file.mv(`products/images/${file.name}`)
-    addqr=`UPDATE products SET  name='${product.Name}' , purchasePrice='${product.purchasePrice}',sellingPrice='${product.sellingPrice}',salesPrice='${product.salesPrice}', mrp='${product.MRP}' ,warranty='${product.Warranty}', image='${file.name}', Brand='${product.Brand}',qty='${product.qty}',HSN='${product.HSN}' ,Tax='${product.Tax}',category='${product.categoryid}' where id='${product.operationid}'`;
-    }
-    else
-    {
+ 
       addqr=`UPDATE products SET  name='${product.Name}' , purchasePrice='${product.purchasePrice}',sellingPrice='${product.sellingPrice}',salesPrice='${product.salesPrice}', mrp='${product.MRP}' ,warranty='${product.Warranty}',  Brand='${product.Brand}',qty='${product.qty}',HSN='${product.HSN}' ,Tax='${product.Tax}',category='${product.categoryid}' where id='${product.operationid}'`;
  
-    }
     con.query(addqr,(err,result)=>{
 
       if(err) throw (err);
     else 
     {
     
-      
+    
+      for(let i=0;i<=5;i++)
+      {
+
+        if(imageblob[i])
+        {
+   
+         if(i==0)
+         {
+          let image=req.files.image
+         
+          image && productimage(image,result.insertId)
+         }
+         else
+         {
+          let image=req.files["image"+(i)] 
+        
+          image && productimage(image,result.insertId)
+         }
+        
+        }
+      }
       categoryattribute=`select * from categoryattribute where  categoryId="${product.categoryid}"`
       con.query(categoryattribute,(err,result1)=>{
         if(err) throw (err)
@@ -192,7 +221,8 @@ router.get('/getProduct',(req,res)=>{
 })
 router.get('/productdetails',(req,res)=>{
 
-  getProduct=`select * from products  where id=${req.query.productId} innerjoin  productimage where id=${req.query.productId} `
+  // getProduct=`select * from products  where id=${req.query.productId} innerjoin  productimage where id=${req.query.productId} `
+  getProduct=` SELECT *,(SELECT group_concat(concat_ws(',', image) separator '; ') FROM productimage WHERE productId = ${req.query.productId}) as image from products where id=${req.query.productId}`
     con.query(getProduct,(err,result)=>{
        if(err) throw (err)
        else
