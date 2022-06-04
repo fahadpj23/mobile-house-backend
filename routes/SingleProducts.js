3779233d6348450addab41aefaddaa60aa8f8e55
fahadpj23 <fahadpj23@gmail.com>
2022-06-04 14:05:21 +0530

    variant

--- routes/SingleProducts.js
+++ routes/SingleProducts.js
@@ -35,7 +35,7 @@ router.get("/singleview",function(req,res)
                       
                         
                         result[0].attributes=attributesarray
-                        /
+                        
                         res.json({"product":result[0]})
                     }
                })
@@ -71,65 +71,28 @@ router.get("/variantproduct",function(req,res)
 {
     let attributesarray=[];
     let variantproduct=[];
- variant=`select * from products where variantid='${req.query.variantid}'    `
- console.log(variant)
- 
- con.query(variant,(err,result)=>{
+    variant=`select *,(SELECT group_concat(concat_ws(',', image) separator '; ') as image FROM productimage WHERE productimage.productId = products.id ) as image from products where variantid='${req.query.variantid}'   `
+    con.query(variant,(err,result)=>{
     if(err) throw (err);
     else
     {
-        result && Object.values(result).map((item,key)=>{
-            getProduct=` SELECT group_concat(concat_ws(',', image) separator '; ') as image FROM productimage WHERE productId = ${item.id} `
-            con.query(getProduct,(err,result1)=>{
-               if(err) throw (err)
-               else
-              {
-
-                result[key].image=result1[0].image
-                getProduct=`select * from productattribute  where productid=${item.id}`
-                
-                con.query(getProduct,(err1,result2)=>{
-                   if(err) throw (err)
-                   else
-                  {
-                   
-                   Object.values(result2).map((item,key1)=>{
-                            attributedetails=`select attributeName,( select value from attributevalue where id=${item.attributeValueId} ) as attributeval from attribute where id=${item.attributeId}`
-                                
-                            con.query(attributedetails,(err2,result2)=>{
-                            if(err2) throw (err2)
-                            else
-                            {  
-                                let att={[result2[0].attributeName]:{attributeId:item.attributeId,attributevalueId:item.attributeValueId,attributevalue:result2[0].attributeval}}
-                                attributesarray.push(att)
-                                result[key].attributes=attributesarray
-                                console.log(result)
-                            }
-                            
-                            if(result2.length ==  key1+1 )
-                            {   
-                              
-                                
-                                item.attributes=attributesarray
-                                if(Object.values(result).length==key+1)
-                                {
-                                    res.json({'variants':result})
-                                }
-                              
-                            }
-                       })
-                   
-                       })
-        
-                  }
+            Object.values(result).map((item,key)=>{
+                productattribute=`select *,(select attributeName from attribute where productattribute.attributeId=attribute.id ) as attributeName ,(select value  from attributevalue where attributevalue.id=productattribute.attributeValueId ) as attributeValue from productattribute where productid=${item.id}`
+               
+                con.query(productattribute,(err1,result1)=>{
+                    if(err1) throw (err1)
+                    else
+                    {
+                        result[key].attributes=result1
+                        
+                        if( Object.values(result).length == key+1)
+                        {
+                            res.send(result)
+                        }
+                        
+                    }
                 })
-              }
-             
             })
-            console.log(Object.values(result).length)
-            console.log(key)
-           
-        })
        
     
     }
