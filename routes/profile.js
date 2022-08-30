65592b9a56b594e970639f4dde15f10c09b470a0
fahadpj23 <fahadpj23@gmail.com>
2022-08-30 17:34:20 +0530

    pprofile

--- routes/profile.js
+++ routes/profile.js
@@ -1,13 +1,13 @@
 const express=require('express')
-
+const validateUserToken=require('../middlewares/WebsiteMiddleware')
 const router = express.Router()
 const con=require('../database')
 
 
 //get the single product detail
-router.get("/MyOrderDetails",function(req,res)
+router.get("/MyOrderDetails",validateUserToken,function(req,res)
 {
-   getMyorder=`SELECT customerorderdetails.orderId,customername,phone,pincode,address,date,customerorderdetails.productid,productimage.image,products.name FROM customerOrder LEFT JOIN customerorderdetails ON customerOrder.orderid = customerorderdetails.orderId LEFT JOIN productimage ON productimage.productId=customerorderdetails.productid LEFT JOIN products ON products.id=customerorderdetails.productid`
+   getMyorder=`SELECT customerorderdetails.orderId,customername,phone,pincode,address,date,customerorderdetails.productid,productimage.image,products.name FROM customerOrder LEFT JOIN customerorderdetails ON customerOrder.orderid = customerorderdetails.orderId LEFT JOIN productimage ON productimage.productId=customerorderdetails.productid LEFT JOIN products ON products.id=customerorderdetails.productid where customerId='${req.user.id}'`
    console.log(getMyorder)
    con.query(getMyorder,(err,result)=>{
     if(err) throw (err)
