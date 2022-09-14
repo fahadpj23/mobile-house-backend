f9be2b7997951c36b6279c49bf8ae4eaf20814a1
fahadpj23 <fahadpj23@gmail.com>
2022-09-14 13:17:02 +0530

    serach

--- routes/ProductListWebsite.js
+++ routes/ProductListWebsite.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const con=require('../database')
 router.get("/viewCategoryProduct",function(req,res)
  {
-    console.log(req.query)
+
     let sortColumn= (req.query.sort=="newestfirst") ? "id" :  "sellingPrice" 
     let sort=(req.query.sort=="Price-Low-to-High") ? "ASC" : "DESC"
     
     selectqr=`SELECT id,name,sellingPrice,salesPrice,mrp,warranty,qty as maxqty,Brand,HSN_code,Tax,category,Description,variantid,(SELECT IF ((SELECT EXISTS(SELECT * FROM productimage WHERE imagePosition = 1 and productimage.productId = products.id) as result) = 1 , (SELECT image FROM productimage WHERE imagePosition = 1 AND productimage.productId = products.id) , (SELECT image FROM productimage WHERE productimage.productId = products.id LIMIT 1) )  ) as image from products where category='${req.query.category}' ORDER BY ${sortColumn} ${sort}  `
-    console.log(selectqr)
+   
     con.query(selectqr,(err,result,fields)=>{
 
       if(err) throw(err);
@@ -20,6 +20,25 @@ router.get("/viewCategoryProduct",function(req,res)
   
 })
 
+// router.get("/ProductList",function(req,res)
+//  {
+
+//     let sortColumn= (req.query.sort=="newestfirst") ? "id" :  "sellingPrice" 
+//     let sort=(req.query.sort=="Price-Low-to-High") ? "ASC" : "DESC"
+    
+//     selectqr=`SELECT id,name,sellingPrice,salesPrice,mrp,warranty,qty as maxqty,Brand,HSN_code,Tax,category,Description,variantid,(SELECT IF ((SELECT EXISTS(SELECT * FROM productimage WHERE imagePosition = 1 and productimage.productId = products.id) as result) = 1 , (SELECT image FROM productimage WHERE imagePosition = 1 AND productimage.productId = products.id) , (SELECT image FROM productimage WHERE productimage.productId = products.id LIMIT 1) )  ) as image from products where (CASE WHEN '${req.query.type}'='Search' THEN name LIKE N'%${req.query.searchValue}%' ELSE ${req.query.type}='${req.query.productList}' END)  ORDER BY ${sortColumn} ${sort}  `
+//     console.log(selectqr)
+//     con.query(selectqr,(err,result,fields)=>{
+
+//       if(err) throw(err);
+//       else
+//       {
+//         console.log(result)
+//           res.send(result)
+//       }
+//   }) 
+  
+// }) 
 router.get("/viewSliderProduct",function(req,res)
  {
    //select column for sorting
