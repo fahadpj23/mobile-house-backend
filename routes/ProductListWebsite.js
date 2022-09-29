3e77372fa56eaae7e7c1bad31e1c79c238cf5b96
fahadpj23 <fahadpj23@gmail.com>
2022-09-29 19:02:37 +0530

    productlist

--- routes/ProductListWebsite.js
+++ routes/ProductListWebsite.js
@@ -8,7 +8,7 @@ router.get("/viewCategoryProduct",function(req,res)
     let sort=(req.query.sort=="Price-Low-to-High") ? "ASC" : "DESC"
     let price=req.query.minprice ? req.query.minprice :0
     let Brand=req.query.BND ? req.query.BND : "'NOBRAND'"
-    console.log(Brand)
+    console.log(req.query.BND ? "sd" : "dsdsd")
     
     // selectqr=`SELECT id,name,sellingPrice,salesPrice,mrp,warranty,qty as maxqty,Brand,HSN_code,Tax,category,Description,variantid,(SELECT IF ((SELECT EXISTS(SELECT * FROM productimage WHERE imagePosition = 1 and productimage.productId = products.id) as result) = 1 , (SELECT image FROM productimage WHERE imagePosition = 1 AND productimage.productId = products.id) , (SELECT image FROM productimage WHERE productimage.productId = products.id LIMIT 1) )  ) as image from products where (CASE WHEN 0!0 THEN category='${req.query.category}' END) ORDER BY ${sortColumn} ${sort}  `
     selectqr=`SELECT id,name,(SELECT MAX(sellingPrice) from products where category='${req.query.category}' )as MaxsellingPrice,(SELECT MAX(salesPrice) from products where category='${req.query.category}' )as MaxsalesPrice,salesPrice,mrp,warranty,qty as maxqty,Brand,HSN_code,Tax,category,Description,variantid,(SELECT IF ((SELECT EXISTS(SELECT * FROM productimage WHERE imagePosition = 1 and productimage.productId = products.id) as result) = 1 , (SELECT image FROM productimage WHERE imagePosition = 1 AND productimage.productId = products.id) , (SELECT image FROM productimage WHERE productimage.productId = products.id LIMIT 1) )  ) as image from products where category='${req.query.category}' and  IF   ("${Brand}"="'NOBRAND'" , category ,Brand in (${Brand})   ) and   products.sellingPrice BETWEEN ${req.query.minprice ? req.query.minprice : 0} AND ${req.query.maxprice ? req.query.maxprice :5000000000000000} and products.salesPrice  BETWEEN ${req.query.minprice ? req.query.minprice : 0} AND ${req.query.maxprice ? req.query.maxprice :5000000000000000}  ORDER BY ${sortColumn} ${sort}  `
@@ -113,11 +113,11 @@ router.get("/productList/category",function(req,res)
     let sortColumn= (req.query.sort=="newestfirst") ? "id" :  "sellingPrice" 
     let sort=(req.query.sort=="Price-Low-to-High") ? "ASC" : "DESC"
     let price=req.query.minprice ? req.query.minprice :0
-    let Brand=req.query.BND ? req.query.BND : "'NOBRAND'"
-    console.log(Brand)
+    let Brand=req.query.BND=="NOBRAND" ? "'NOBRAND'"   : req.query.BND 
+    
     
     // selectqr=`SELECT id,name,sellingPrice,salesPrice,mrp,warranty,qty as maxqty,Brand,HSN_code,Tax,category,Description,variantid,(SELECT IF ((SELECT EXISTS(SELECT * FROM productimage WHERE imagePosition = 1 and productimage.productId = products.id) as result) = 1 , (SELECT image FROM productimage WHERE imagePosition = 1 AND productimage.productId = products.id) , (SELECT image FROM productimage WHERE productimage.productId = products.id LIMIT 1) )  ) as image from products where (CASE WHEN 0!0 THEN category='${req.query.category}' END) ORDER BY ${sortColumn} ${sort}  `
-    selectqr=`SELECT id,name,(SELECT MAX(sellingPrice) from products where category='${req.query.category}' )as MaxsellingPrice,(SELECT MAX(salesPrice) from products where category='${req.query.category}' )as MaxsalesPrice,salesPrice,mrp,warranty,qty as maxqty,Brand,HSN_code,Tax,category,Description,variantid,(SELECT IF ((SELECT EXISTS(SELECT * FROM productimage WHERE imagePosition = 1 and productimage.productId = products.id) as result) = 1 , (SELECT image FROM productimage WHERE imagePosition = 1 AND productimage.productId = products.id) , (SELECT image FROM productimage WHERE productimage.productId = products.id LIMIT 1) )  ) as image from products where category='${req.query.category}' and  IF   ("${Brand}"="'NOBRAND'" , category ,Brand in (${Brand})   ) and   products.sellingPrice BETWEEN ${req.query.minprice ? req.query.minprice : 0} AND ${req.query.maxprice ? req.query.maxprice :5000000000000000} and products.salesPrice  BETWEEN ${req.query.minprice ? req.query.minprice : 0} AND ${req.query.maxprice ? req.query.maxprice :5000000000000000}  ORDER BY ${sortColumn} ${sort}  `
+    selectqr=`SELECT id, (SELECT COUNT(*) from products where category='${req.query.category}' and  IF   ("${Brand}"="'NOBRAND'" , category='${req.query.category}' ,Brand in (${Brand})   ) and   products.sellingPrice BETWEEN ${req.query.minprice ? req.query.minprice : 0} AND ${req.query.maxprice ? req.query.maxprice :5000000000000000} and products.salesPrice  BETWEEN ${req.query.minprice ? req.query.minprice : 0} AND ${req.query.maxprice ? req.query.maxprice :5000000000000000} ) as count, name,(SELECT MAX(sellingPrice) from products where category='${req.query.category}' )as MaxsellingPrice,(SELECT MAX(salesPrice) from products where category='${req.query.category}' )as MaxsalesPrice,salesPrice,mrp,warranty,qty as maxqty,Brand,HSN_code,Tax,category,Description,variantid,(SELECT IF ((SELECT EXISTS(SELECT * FROM productimage WHERE imagePosition = 1 and productimage.productId = products.id) as result) = 1 , (SELECT image FROM productimage WHERE imagePosition = 1 AND productimage.productId = products.id) , (SELECT image FROM productimage WHERE productimage.productId = products.id LIMIT 1) )  ) as image from products where category='${req.query.category}' and  IF   ("${Brand}"="'NOBRAND'" , category='${req.query.category}' ,Brand in (${Brand})   ) and   products.sellingPrice BETWEEN ${req.query.minprice ? req.query.minprice : 0} AND ${req.query.maxprice ? req.query.maxprice :5000000000000000} and products.salesPrice  BETWEEN ${req.query.minprice ? req.query.minprice : 0} AND ${req.query.maxprice ? req.query.maxprice :5000000000000000}  ORDER BY ${sortColumn} ${sort} LIMIT ${(+req.query.PageNo-1) * 10}, 30 `
     console.log(selectqr)
     con.query(selectqr,(err,result,fields)=>{
 
@@ -131,7 +131,7 @@ router.get("/productList/category",function(req,res)
           if(err1) throw(err1);
           else
           {
-          res.json({products:result,Brand:result1})
+          res.json({products:result,Brand:result1,TotalProduct:result[0]?.count??0})
           }
         })
       }
@@ -139,25 +139,7 @@ router.get("/productList/category",function(req,res)
   
 })
 
-// router.get("/ProductList",function(req,res)
-//  {
 
-//     let sortColumn= (req.query.sort=="newestfirst") ? "id" :  "sellingPrice" 
-//     let sort=(req.query.sort=="Price-Low-to-High") ? "ASC" : "DESC"
-    
-//     selectqr=`SELECT id,name,sellingPrice,salesPrice,mrp,warranty,qty as maxqty,Brand,HSN_code,Tax,category,Description,variantid,(SELECT IF ((SELECT EXISTS(SELECT * FROM productimage WHERE imagePosition = 1 and productimage.productId = products.id) as result) = 1 , (SELECT image FROM productimage WHERE imagePosition = 1 AND productimage.productId = products.id) , (SELECT image FROM productimage WHERE productimage.productId = products.id LIMIT 1) )  ) as image from products where (CASE WHEN '${req.query.type}'='Search' THEN name LIKE N'%${req.query.searchValue}%' ELSE ${req.query.type}='${req.query.productList}' END)  ORDER BY ${sortColumn} ${sort}  `
-//     console.log(selectqr)
-//     con.query(selectqr,(err,result,fields)=>{
-
-//       if(err) throw(err);
-//       else
-//       {
-//         console.log(result)
-//           res.send(result)
-//       }
-//   }) 
-  
-// }) 
 router.get("/productList/productCategory",function(req,res)
  {
    //select column for sorting
@@ -165,7 +147,7 @@ router.get("/productList/productCategory",function(req,res)
 
     let sort=(req.query.sort=="Price-Low-to-High") ? "ASC" : "DESC"
 
-          headProduct=`SELECT  (SELECT id from products where products.id=headproduct.productid) as id ,(SELECT name from products where products.id=headproduct.productid) as name,(SELECT MAX(sellingPrice) from products where products.id=headproduct.productid )as MaxsellingPrice,(SELECT MAX(salesPrice) from products where products.id=headproduct.productid )as MaxsalesPrice, (SELECT sellingPrice from products where products.id=headproduct.productid) as sellingPrice, (SELECT salesPrice from products where products.id=headproduct.productid) as salesPrice ,(SELECT mrp from products where products.id=headproduct.productid) as mrp ,(SELECT variantid from products where products.id=headproduct.productid) as variantid ,(SELECT image from productimage where productimage.productId=headproduct.productid LIMIT 1) as image from headproduct where HeadId=${req.query.productCategory}  ORDER BY ${sortColumn} ${sort};`
+          headProduct=`SELECT  (SELECT id from products where products.id=headproduct.productid) as id ,(SELECT name from products where products.id=headproduct.productid) as name,(SELECT MAX(sellingPrice) from products where products.id=headproduct.productid )as MaxsellingPrice,(SELECT MAX(salesPrice) from products where products.id=headproduct.productid )as MaxsalesPrice, (SELECT sellingPrice from products where products.id=headproduct.productid) as sellingPrice, (SELECT salesPrice from products where products.id=headproduct.productid) as salesPrice ,(SELECT mrp from products where products.id=headproduct.productid) as mrp ,(SELECT variantid from products where products.id=headproduct.productid) as variantid ,(SELECT image from productimage where productimage.productId=headproduct.productid LIMIT 1) as image from headproduct where HeadId=${req.query.productCategory}  ORDER BY ${sortColumn} ${sort}  LIMIT ${(+req.query.PageNo-1) * 10}, 30`
          console.log(headProduct)
           con.query(headProduct,(err,result)=>{
               if(err)  throw (err)
