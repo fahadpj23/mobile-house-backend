877cc1d200f38ef395639a2e8461aa892b47d6f7
fahadpj23 <fahadpj23@gmail.com>
2022-10-08 11:41:23 +0530

    before payemnt

--- routes/routes/OrderProduct.js
+++
@@ -1,7 +0,0 @@
-const express=require('express')
-var bodyParser=require("body-parser");
-var jsonParser=bodyParser.json();
-const router = express.Router()
-const con=require('../database')
-
-module.exports=router; 
